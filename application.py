926637b151042a0ba6e1919a9e5def611988387f
Neelay Ambalkar <ramba@LAPTOP-0MM5VL1H>
2026-05-16 14:06:25 +0530

    commit

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,request
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
 from pipeline.prediction_pipeline import hybrid_recommendation
 
